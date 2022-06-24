1567b247dac7e8a0015745e9e355977369c42a68
Abhishek32971 <abhishek32971@gmail.com>
2022-06-24 22:38:24 +0530

    whatever

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,5 @@
-# Dictionaries
-
-filename = "dataset/mbox-short.txt"
+counts = dict()
+names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
+for name in names :
+    print(name,counts.get(name, 0)) 
+print(counts)
